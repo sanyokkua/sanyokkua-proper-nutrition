db76cdbbad1962b0a26911f1bbd075c6923ba01e
Alexander Kostenko <sanyokkua@gmail.com>
2018-12-07 17:07:36 +0200

    Added first project files

---
+++ src/main/java/com/kostenko/pp/pp/PpApplication.java
@@ -0,0 +1,12 @@
+package com.kostenko.pp.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kostenko/pp/pp/PpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kostenko.pp.pp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
