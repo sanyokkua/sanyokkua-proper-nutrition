eb26fd72dc5964280f51aeb8760442fea2a286ab
Alexander Kostenko <sanyokkua@gmail.com>
2018-12-07 18:50:11 +0200

    Added libraries for development

--- src/main/java/com/kostenko/pp/PpApplication.java
+++ src/main/java/com/kostenko/pp/PpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PpApplication.class, args);
+    }
 }
